79ac3c1fbd2ee0df6561ab91b13f44a327608b0b
61shad <130103308+61shad@users.noreply.github.com>
2024-05-30 20:26:02 +1200

    instruction done

--- 02_instruction.py
+++ 02_instruction.py
@@ -17,30 +17,31 @@ def yes_no(question):
 def instruction():
     print('''
 
-        *** Instructions ***
+    *** Instructions ***
 
-        At first step you should choose what operator you want to work with
-        (addition, subtraction, multiplication and division).
+    At first step you should choose what operator you want to work with
+    (addition, subtraction, multiplication and division).
 
-        Next have to choose the level of difficulty you want to do, 
-        level of difficulty are (Easy, Medium and Hard) 
+    Next you should choose the number of questions you want to do, 
+    for the infinite questions you can write <inf>.
 
-        After that, you should choose the number of questions you want to do, 
-        for the infinite questions you can write <inf>.
+    After that, you have to choose the level of difficulty you want to do, 
+    level of difficulty are (Easy, Medium, Hard and expert). When you choose the
+    level of the questions then the questions woould be asked. 
 
-        If you wanted to exit the code, just write <i am done>. Keep in mind that 
-        you can only exit when you are answering the questions and you cannot exit
-        the quiz in other stages.  
+    If you wanted to exit the code, just write <i am done>. Keep in mind that 
+    you can only exit when you are answering the questions and you cannot exit
+    the quiz in other stages. Also please use lower case not apper case.
 
-        At the end you can see your quiz history. If you wanted to see the history
-        write yes and then you can check the history.
+    At the end you can see your quiz history. If you wanted to see the history
+    write yes and then you can check the history. But if you did not wanted to see
+    the history then type no. It will skip the history and shows you the statistics. 
 
+    Get Ready!!
+    See you in the quiz!
+    Good luck!
+        ''')
 
-        Get Ready!!
-        see you in the quiz!
-
-        Good luck.
-            ''')
 # main routine goes here
 print()
 print("📝📝 Math  📝📝")
